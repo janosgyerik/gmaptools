30a6169bc1c2a83cb9bcdfdc8158df084cda5533
Janos Gyerik <info@titan2x.com>
2012-09-16 23:03:52 +0200

    added backbone files and basic gmaptools templates

---
+++ static/js/gmaptools.js
@@ -0,0 +1,15 @@
+/*!
+ * gmaptools JavaScript Library v0.1
+ * http://.../
+ *
+ * Copyright 2012, Janos Gyerik
+ * http://.../license
+ *
+ * Date: Sun Sep 16 23:00:33 CEST 2012
+ */
+
+
+$(document).ready(function() {
+});
+
+// eof

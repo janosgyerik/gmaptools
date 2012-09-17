d06c41d8e3930546d4dc0e60d2df2a617ee803b2
Janos Gyerik <info@titan2x.com>
2012-09-17 22:56:46 +0200

    added latlon tool

--- static/js/gmaptools.js
+++ static/js/gmaptools.js
@@ -11,11 +11,11 @@
 
 var map;
 
-function initialize() {
+function initialize_map() {
     var latlng;
     if (google.loader.ClientLocation) {
-        lat = google.loader.ClientLocation.latitude;
-        lng = google.loader.ClientLocation.longitude;
+        var lat = google.loader.ClientLocation.latitude;
+        var lng = google.loader.ClientLocation.longitude;
         latlng = new google.maps.LatLng(lat, lng);
     }
     else {
@@ -29,15 +29,77 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map'), options);
 
     google.maps.event.addListener(map, 'center_changed', centerChanged);
+    google.maps.event.addListener(map, 'zoom_changed', centerChanged);
     centerChanged();
+}
 
-    //map.panTo(latlng);
-    //map.panToBounds(bounds);
-    //map.fitBounds(bounds);
+function new_point(lat, lon) {
+    return new google.maps.LatLng(lat, lon);
+}
+
+function pan_to(point) {
+    map.panTo(point);
+}
+
+function drop_pin(point) {
+    pan_to(point);
+    // TODO: use factory to keep reference to markers
+    var marker = new google.maps.Marker({
+        position: point,
+        map: map
+    });
+}
+
+function home() {
+    if (google.loader.ClientLocation) {
+        var lat = google.loader.ClientLocation.latitude;
+        var lon = google.loader.ClientLocation.longitude;
+        pan_to(new_point(lat, lon));
+    }
+}
+
+function initialize_latlon_tool() {
+    var form = $('#latlon-tool');
+    var lat_input = form.find('.lat');
+    var lon_input = form.find('.lon');
+
+    function get_point() {
+        var lat = lat_input.val();
+        var lon = lon_input.val();
+        return lat && lon ? new_point(lat, lon) : map.getCenter();
+    }
+
+    var btn_goto = form.find('.btn-goto');
+    btn_goto.bind('click', function() {
+        pan_to(get_point());
+    });
+
+    var btn_pin = form.find('.btn-pin');
+    btn_pin.bind('click', function() {
+        drop_pin(get_point());
+    });
+
+    var btn_home = form.find('.btn-home');
+    btn_home.bind('click', home);
+
+    var btn_here = form.find('.btn-here');
+    btn_here.bind('click', function() {
+        var point = map.getCenter();
+        lat_input.val(point.lat());
+        lon_input.val(point.lng());
+    });
+}
+
+function initialize() {
+    initialize_map();
+    initialize_latlon_tool();
 }
 
 function centerChanged() {
     App.mapStats.update(map);
+    //map.panTo(latlng);
+    //map.panToBounds(bounds);
+    //map.fitBounds(bounds);
 }
 
 
